1ade58e3acc52d4cd4a01b3d8edc95bc43db574c
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-24 10:56:18 +0000

    Month drop down with numeric options to pass implemented

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,4 +3,8 @@ feature 'Page display' do
     visit('/')
     expect(page).to have_content "When is your birthday?"
   end
+  scenario 'Page has drop-down menu for months' do
+    visit('/')
+    expect(page).to have_content("April")
+  end
 end
